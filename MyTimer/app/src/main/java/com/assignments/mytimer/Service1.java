c89d73173db1960b586479d82ba7bb792ceb9e7b
gervanpn <gervann@yahoo.ca>
2020-08-12 17:16:22 -0400

    Latest updates Aug 12 Assignment

--- MyTimer/app/src/main/java/com/assignments/mytimer/Service1.java
+++ MyTimer/app/src/main/java/com/assignments/mytimer/Service1.java
@@ -3,9 +3,7 @@ package com.assignments.mytimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 
